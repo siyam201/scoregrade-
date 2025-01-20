45d2ea4975220a8a4811f1aedff56af91dd93094
siyam201 <mdseium74@gmail.com>
2025-01-21 00:03:24 +0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,18 @@
+function gradeScore() {
+    let scor = document.getElementById('scoreInput').value;
+    let g = ''; 
+    let f = '';
+    if (scor >= 80 && scor <= 100) {
+        g = 'PASSDE: A';
+    } else if (scor >= 50 && scor <= 79) {
+        g = 'PASSDE: B';
+    } else if (scor >= 33 && scor <= 49) {
+        g = 'PASSDE: C';
+    } else if (scor >= 0 && scor <= 32) {
+        f = 'FALL: F';
+    } else {
+        g = 'Invalid Score';
+    }
+    document.getElementById('f').innerText = f;
+    document.getElementById('result').innerText = g;
+}
